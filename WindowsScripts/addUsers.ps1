01be55af41979d3c02b76daeb51d787ae108bf34
Catchco1 <cag107@gmail.com>
2019-04-21 00:09:07 -0400

    Reorganize and fix addUsers.ps1

---
+++ WindowsScripts/addUsers.ps1
@@ -0,0 +1,21 @@
+﻿$ADUsers = Import-csv C:\exerciseUsers.csv
+
+foreach($User in $ADUsers)
+{
+    $username = $User.first.ToLower()[0] + $User.last.ToLower()
+    $password = $User.Password
+    $fullName = $User.first + ' ' + $User.last
+    if($User.Role -eq "IT Staff" -or $User.Role -eq "Production"){
+        New-ADUser -Name $fullName `
+        -AccountPassword(ConvertTo-SecureString $password -AsPlainText -Force) `
+        -DisplayName $fullName `
+        -Enabled $true `
+        -SamAccountName $username `
+        -givenname $User.first `
+        -surname $User.last `
+        -userprincipalname ($username + "@production.team2.tu")
+        if($User.Role -eq "IT Staff"){
+            Add-ADGroupMember -Identity "Domain Admins" -Member $username
+        }
+    }
+}
